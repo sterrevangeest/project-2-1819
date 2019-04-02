2cca0aa4a7f186123f03f9b885e84369db548a5e
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-04-02 22:02:50 +0200

    update

--- README.md
+++ README.md
@@ -1,36 +1,6 @@
 # Project 2 @cmda-minor-web · 2018-2019
 
-## Aanbevelingen voor een robuste, toegankelijke en snelle web toepassing
+Van laag naar hoog:
+`temperature`
 
-Het project vindt plaats bij in ons eigen lokaal. Maandagochtend is om 9.30 uur de kickoff (bij de oprachtgever), vrijdag zijn de presentaties van de resultaten (bij de opdrachtgever). In een week bouwt iedere student een eigen prototype ter onderbouwing van aanbevelingen aan de opdrachtgevers (Valtech, Mirabeau & CMD Amsterdam). De aanbevelingen betreffen het verbeteren van toegankelijkheid en performance van de web sites / applicaties. Technieken geleerd bij [Browser Technologies](https://github.com/cmda-minor-web/browser-technologies-1819) en [Performance Matters](https://github.com/cmda-minor-web/performance-matters-1819) worden toegepast bij het bouwen van de de prototypes en et schrijven van de aanbevelingen.
-
-## Programma
-Maandag - 9.30 - kickoff
-Woensdag - voortgangbespreking
-Vrijdag presentatie
-
-## Werkwijze en Criteria
-### Werkwijze
-Full-time week werken aan optimaliseren van toegankelijkheid en performance van een website. Vrijdag is de pitch! Student laat zien dat hij/zij de vakken [Browser Technologies](https://github.com/cmda-minor-web/browser-technologies-1819) en [Performance Matters](https://github.com/cmda-minor-web/performance-matters-1819) begrijpt en beheerst.
-
-Iedere dag zijn er coaches aanwezig voor coaching en feedback. We sluiten de dag af met een stand up, om de status door te spreken. Waar ben je mee bezig? Loop je ergens vast of heb je hulp nodig? Waar sta je?
-
-### Beoordeling
-Tijdens de beoordeling krijg je feedback op het resultaat en op je functioneren. De vakdocenten kijken naar je code en beoordelen In hoeverre je in het project laten zien dat je de bijhorende vakken beheerst en goed hebt toegepast. De opdrachtgver is geïnteresseerd in hoeverre je oplossing voldoet aan de eisen die zijzelf stellen aan hun producten. Misschien laat je wel wat zien waar ze zelf nog niet aan gedacht hebben?
-
-Het project telt als AVV mee met de Meesterproef.
-
-### Feedback over functioneren
-Je hebt een leergierig, gedreven en zelfredzame houding nodig om de minor te kunnen halen. Welke vaardigheden heb je laten zien? Onderzoekend vermogen? Creativiteit? Conceptueel? In hoeverre komen je houding en verworven vaardigheden overeen met wat een frontender in de praktijk nodig heeft?
-
-### Browser Technologies
-In het vak [Browser Technologies](https://github.com/cmda-minor-web/browser-technologies-1819)) heb je geleerd wat de core functionalitiet van een web toepassing is en hoe je deze functionaliteit met progressive enhancement kan opbouwen tot een robuste web teopassing. Ook heb je geleerd hoe hiermee de toegankelijkheid van een web toepassing kan worden verbeterd. 
-
-### Performance Matters
-In het vak [Performance Matters](https://github.com/cmda-minor-web/performance-matters-1819) heb je geleerd hoe je een web toepassing (ogenschijnlijk) sneller kan maken en ook offline kan laten werken. Hiermee verbeter je de gebruikservaring van de web toepassing in alle omstandigheden.  
-
-
-### Oplevering & criteria
-- Presentatie met je bevindingen bij de opdrachtgever. 
-- Github met je code en readme.
-
+`LUX light`: laag is relatief donker, hoog is relatief licht
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ console.log("test");
 
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser());
+
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 const fetch = require("node-fetch");
@@ -9,16 +12,90 @@ var ejs = require("ejs");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", function(request, response) {
+// app.get("/", function(req, res) {
+//   res.render("pages/index", {});
+// });
+
+// app.get("/", function(req, res) {
+//   fetch("http://mirabeau.denniswegereef.nl/api/v1/rooms")
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(json) {
+//       var data = json.data;
+//
+//       // var data = data.sort(function(a, b) {
+//       //   var a = a.measurements.temperature;
+//       //   var b = b.measurements.temperature;
+//       //   if (filterId === "warm") {
+//       //     return a - b;
+//       //   } else if (filterId === "koud") {
+//       //     return b - a;
+//       //   }
+//       // });
+//
+//       var measurements = data.find(function(item) {
+//         console.log(Object.keys(item.measurements));
+//         var keys = Object.keys(item.measurements);
+//
+//         console.log(keys);
+//         return keys;
+//       });
+//
+//       delete measurements.measurements.batt;
+//       delete measurements.measurements.uv_index;
+//       delete measurements.measurements.occupancy;
+//
+//       console.log("M.M", measurements.measurements);
+//
+//       var data = data.map(function(item) {
+//         return {
+//           roomName: item.room_name,
+//           url: item.room_name.toLowerCase().replace(/ /g, "_"),
+//           measurements: item.measurements
+//         };
+//       });
+//
+//       res.render("pages/index", {
+//         data: data,
+//         measurements: measurements.measurements
+//       });
+//     });
+// });
+
+app.get("/form", function(req, res) {
+  res.render("pages/form");
+});
+
+app.get("/index/:id", function(req, res) {
+  var filterId = req.params.id;
   fetch("http://mirabeau.denniswegereef.nl/api/v1/rooms")
-    .then(function(res) {
-      return res.json();
+    .then(function(response) {
+      return response.json();
     })
     .then(function(json) {
       var data = json.data;
+
       var measurements = data.map(function(item) {
         return item.measurements;
       });
+
+      var measurementsKeys = data.find(function(item) {
+        console.log(Object.keys(item.measurements));
+        var keys = Object.keys(item.measurements);
+        return keys;
+      });
+
+      delete measurementsKeys.measurements.batt;
+      delete measurementsKeys.measurements.uv_index;
+      delete measurementsKeys.measurements.occupancy;
+
+      var data = data.sort(function(a, b) {
+        var a = a.measurements[filterId];
+        var b = b.measurements[filterId];
+        return a - b;
+      });
+
       var data = data.map(function(item) {
         return {
           roomName: item.room_name,
@@ -26,36 +103,67 @@ app.get("/", function(request, response) {
           measurements: item.measurements
         };
       });
-      response.render("pages/index", {
-        data: data
+
+      res.render("pages/index", {
+        data: data,
+        measurements: measurementsKeys.measurements,
+        filterId: filterId,
+        meas: measurements
       });
     });
 });
 
-app.get("/room/:id", function(request, response) {
-  console.log(request.params);
-
-  response.render("pages/room", {});
-  console.log(
-    "url",
-    "http://mirabeau.denniswegereef.nl/api/v1/rooms/" + request.params.id
-  );
-  fetch("http://mirabeau.denniswegereef.nl/api/v1/rooms/" + request.params.id)
-    .then(function(res) {
-      return res.json();
+app.post("/postform", function(req, res) {
+  fetch("http://mirabeau.denniswegereef.nl/api/v1/rooms")
+    .then(function(response) {
+      return response.json();
     })
     .then(function(json) {
-      console.log(json);
-      // var data = json.data;
-      // var measurements = data.map(function(item) {
-      //   return item.measurements;
-      // });
-      // console.log(measurements);
-      // response.render("pages/index", {
-      //   data: data,
-      //   measurements: measurements
-      // });
+      var data = json.data;
+      console.log(req.body);
+      var temperatures = data.map(function(item) {
+        return item.measurements.temperature;
+      });
+      var mic_levels = data.map(function(item) {
+        return item.measurements.mic_level;
+      });
+      var ambient_light = data.map(function(item) {
+        return item.measurements.ambient_light;
+      });
+
+      var temperatureGoal = req.body.temperature;
+
+      var closest = temperatures.reduce(function(prev, curr) {
+        return Math.abs(curr - temperatureGoal) <
+          Math.abs(prev - temperatureGoal)
+          ? curr
+          : prev;
+      });
+
+      console.log(closest);
+
+      console.log(temperatures);
+      // console.log(mic_levels);
+      // console.log(ambient_light);
+
+      res.render("pages/nieuweindex", {});
     });
 });
+
+// app.get("/room/:id", function(req, res) {
+//   console.log(req.params);
+//
+//   fetch("http://mirabeau.denniswegereef.nl/api/v1/room/" + req.params.id)
+//     .then(function(res) {
+//       return res.json();
+//     })
+//     .then(function(json) {
+//       var data = json.data;
+//       console.log(data);
+//       res.render("pages/room", {
+//         data: data
+//       });
+//     });
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
