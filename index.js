a089a224a0ed38c44637d3df8d53120d1f9f0413
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-04-03 17:15:09 +0200

    update

--- index.js
+++ index.js
@@ -81,7 +81,6 @@ app.get("/index/:id", function(req, res) {
       });
 
       var measurementsKeys = data.find(function(item) {
-        console.log(Object.keys(item.measurements));
         var keys = Object.keys(item.measurements);
         return keys;
       });
@@ -120,33 +119,157 @@ app.post("/postform", function(req, res) {
     })
     .then(function(json) {
       var data = json.data;
-      console.log(req.body);
+
+      var temperatureGoal = req.body.temperature;
+      var mic_levelGoal = req.body.mic_level;
+      var ambient_lightGoal = req.body.ambient_light;
+
       var temperatures = data.map(function(item) {
-        return item.measurements.temperature;
+        return item.measurements.temperature / 1000;
       });
       var mic_levels = data.map(function(item) {
         return item.measurements.mic_level;
       });
       var ambient_light = data.map(function(item) {
-        return item.measurements.ambient_light;
+        return item.measurements.ambient_light / 1000;
       });
 
-      var temperatureGoal = req.body.temperature;
-
-      var closest = temperatures.reduce(function(prev, curr) {
-        return Math.abs(curr - temperatureGoal) <
-          Math.abs(prev - temperatureGoal)
-          ? curr
-          : prev;
+      var mic_levelPercentage = temperatures.map(function(item) {
+        var value = ((mic_levelGoal - item) / item) * 100;
+        if (value < 0) {
+          return Math.abs(value);
+        }
+        return value;
       });
 
-      console.log(closest);
+      var temperaturePercentage = temperatures.map(function(item) {
+        var value = ((temperatureGoal - item) / item) * 100;
+        if (value < 0) {
+          return Math.abs(value);
+        }
+        return value;
+      });
 
-      console.log(temperatures);
-      // console.log(mic_levels);
-      // console.log(ambient_light);
+      var ambient_lightPercentage = ambient_light.map(function(item) {
+        var value = ((ambient_lightGoal - item) / item) * 100;
+        if (value < 0) {
+          return Math.abs(value);
+        }
+        return value;
+      });
 
-      res.render("pages/nieuweindex", {});
+      var xs = data.map(function(rooms) {
+        return rooms.room_name;
+      });
+
+      var sorted = xs
+        .map(function(x, i) {
+          return {
+            name: x,
+            temperatureScore: temperaturePercentage[i],
+            temperature: temperatures[i]
+          };
+        })
+        .sort(function(a, b) {
+          var c = a.temperatureScore - b.temperatureScore;
+          return c ? c : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+        })
+        .map(function(x, i) {
+          x.temperatureRank = i;
+          return x;
+        });
+
+      var sorted2 = xs
+        .map(function(x, i) {
+          return {
+            name: x,
+            ambient_lightScore: ambient_lightPercentage[i],
+            ambient_light: ambient_light[i]
+          };
+        })
+        .sort(function(a, b) {
+          var c = a.ambient_lightScore - b.ambient_lightScore;
+          return c ? c : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+        })
+        .map(function(x, i) {
+          // data[i].ambientlightRank = i;
+          x.ambientlightRank = i;
+          return x;
+        });
+
+      var sorted3 = xs
+        .map(function(x, i) {
+          return {
+            name: x,
+            mic_levelScore: mic_levelPercentage[i],
+            mic_level: mic_levels[i]
+          };
+        })
+        .sort(function(a, b) {
+          var c = a.mic_levelScore - b.mic_levelScore;
+          return c ? c : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+        })
+        .map(function(x, i) {
+          x.mic_levelRank = i;
+          return x;
+        });
+      // console.log(sorted);
+      // console.log(sorted2);
+      // console.log(sorted3);
+
+      var combine = sorted.concat(sorted2, sorted3);
+
+      const groupBy = key => array =>
+        array.reduce((objectsByKeyValue, obj) => {
+          const value = obj[key];
+          objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(
+            obj
+          );
+          return objectsByKeyValue;
+        }, {});
+
+      const groupByBrand = groupBy("name");
+
+      var arrObj = [groupByBrand(combine)];
+
+      var arr = [];
+
+      var testen = arrObj.map(function(item, i) {
+        console.log(i);
+        for (var i = 0; i < xs.length; i++) {
+          // console.log(xs[i]);
+          var bell = item[xs[i]];
+          var resultObject = bell.reduce(function(result, currentObject) {
+            for (var key in currentObject) {
+              if (currentObject.hasOwnProperty(key)) {
+                result[key] = currentObject[key];
+              }
+            }
+            return result;
+          }, {});
+
+          arr.push(resultObject);
+        }
+        // return resultObject;
+      });
+      // console.log("arr", arr);
+
+      var newranking = arr.map(function(item, i) {
+        var ranking =
+          item.temperatureRank + item.mic_levelRank + item.ambientlightRank;
+        arr[i].ranking = ranking;
+        return arr;
+      });
+
+      // console.log(arr);
+
+      var sort = arr.sort((a, b) => (a.ranking > b.ranking ? 1 : -1));
+
+      console.log(sort);
+
+      res.render("pages/nieuweindex", {
+        data: sort
+      });
     });
 });
 
